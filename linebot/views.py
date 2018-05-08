3f6a711182edf7abe1cde328cefb32bbd754f0cf
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-05-08 14:47:48 +0900

    id=

--- linebot/views.py
+++ linebot/views.py
@@ -4,7 +4,7 @@ import paramiko
 
 def index(request):
     if request.method == 'GET':
-        k = "https://drive.google.com/open?id=1_mCedTUusHIMJzvd8fUy9Q8d7Zkp2gtK"
+        k = "http://drive.google.com/uc?export=view&id=1_mCedTUusHIMJzvd8fUy9Q8d7Zkp2gtK"
         client = paramiko.SSHClient() 
         client.load_system_host_keys(k) 
         client.connect('ark-cg.com')
